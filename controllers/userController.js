15ffc342605dffa72906636fe1c00d212a198aa0
Doğuhan İlter <doguhannilt@gmail.com>
2024-01-10 08:46:24 +0300

    Log in controller, postman check, try-catch

--- controllers/userController.js
+++ controllers/userController.js
@@ -44,5 +44,47 @@ const signupUser = async(req,res) => {
     }
 }
 
+const loginUser = async (req, res) => {
+    try {
+        const { 
+            username, 
+            password } = req.body;
 
-export {signupUser}
+        // Kullanıcıyı bul
+        let user;
+        try {
+            user = await User.findOne({ username });
+        } catch (error) {
+            console.log(error.message);
+            return res.status(500).json({ message: "Internal" });
+        }
+
+        // Kullanıcı kontrolü
+        if (!user) {
+            return res.status(400).json({ message: "There is no such a user" });
+        }
+
+        // Şifre kontrolü
+        const isPaswCorr = await bcrypt.compare(password, user.password);
+
+        if (!isPaswCorr) {
+            return res.status(400).json({ message: "Password is not correct" });
+        }
+
+        // Token oluştur ve çereze ekle
+        generateTokenandSetcookie(user._id, res);
+
+        // Başarılı cevap
+        res.status(200).json({
+            _id: user._id,
+            name: user.username,
+            email: user.email,
+            username: user.username,
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+export { signupUser, loginUser };
