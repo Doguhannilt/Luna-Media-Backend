83f29b5f39754b987c131347c97da71b2a27e0e0
Doğuhan İlter <doguhannilt@gmail.com>
2024-01-10 10:14:07 +0300

    Update controller, postman checked

--- controllers/userController.js
+++ controllers/userController.js
@@ -98,6 +98,7 @@ const logoutUser = (req,res) => {
             {message: error.message})
     }
 }
+
 const Followorunfollow = async (req, res) => {
     try {
         const { id } = req.params;
@@ -146,9 +147,46 @@ const Followorunfollow = async (req, res) => {
     }
 };
 
+const updateUser =  async (req,res) =>{
+    const 
+    {   name, 
+        email, 
+        username, 
+        password, 
+        profilePic, 
+        bio} = req.body
+    const userId = req.user._id
+    try {
+        let user = await User.findById(userId)
+        if(!user) return res.status(400).json({message: 'User not found'})
+
+        if(req.params.id !== userId.toString()) return res.status(400).json({
+            message: "You cannot update other user's profile"
+        })
+
+        if (password) {
+            const salt = await bcrypt.genSalt(10)
+            const hashedpass = await bcrypt.hash(password, salt)
+            user.password = hashedpass
+        }
+
+        user.name = name || user.name
+        user.email = email || user.email
+        user.username = username || user.username
+        user.profilePic = profilePic || user.profilePic
+        user.bio = bio || user.bio
+       
+        user = await user.save()
+        
+        res.status(200).json({message: "Profile updated sucess"})
+    }  catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
 
 export { 
     signupUser, 
     loginUser,
     logoutUser, 
-    Followorunfollow }
+    Followorunfollow,
+    updateUser }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import  { signupUser, loginUser, logoutUser, Followorunfollow }  from '../controllers/userController.js';
+import  { signupUser, loginUser, logoutUser, Followorunfollow, updateUser }  from '../controllers/userController.js';
 import protectRoute from '../middlewares/protectRoute.js';
 
 const router = express.Router()
@@ -9,5 +9,6 @@ router.post("/signup",signupUser )
 router.post("/login", loginUser)
 router.post("/logout",logoutUser)
 router.post("/follow/:id",protectRoute ,Followorunfollow)
+router.post("/update/:id", protectRoute , updateUser)
 
 export default router;
